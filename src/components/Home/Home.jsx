8a195b142b1faf86b62319f619925ed4f553a562
Sayalics23154 <sayalipadole.cse23@sbjit.edu.in>
2025-09-10 21:00:00 +0530

    Save8uuu

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
     <div>
       <img src="https://media.geeksforgeeks.org/wp-content/uploads/20230926163338/Java-Full-Stack.png" alt="Full Stack" className='w-full' />
       <br /><br />
-      <p style={{color:'red'}}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime excepturi cumque quos similique, delectus repellendus, reiciendis animi saepe voluptate temporibus sapiente harum aliquam quia, alias exercitationem minima soluta deserunt unde.</p>
+      <p style={{color:'red'}}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime excepturi cumque quos similique, delectus repellendus, reiciendis animi saepe voluptate temporibus sapiente harum aliquam quia, alias exercitationem minima soluta deserunt unde.</p>
       <PropsPractice abc='home' xyz='home.com' />
     </div>
   )
